cb36e06d6062171ec11308f4b18a64bd9dcd7351
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-03-16 14:29:43 +0300

    tile issue reproduction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { ExpandableTile, TileAboveTheFoldContent } from '@carbon/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ExpandableTile>
+            <TileAboveTheFoldContent>
+                <button>Hello, Carbon!</button>
+            </TileAboveTheFoldContent>
+        </ExpandableTile>
+    );
 }
 
 export default App;
